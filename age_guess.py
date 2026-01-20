16b88ba1fe1ba954dd9a70cdd2980e443afbf087
Tate Fishburn <d00444216@utahtech.edu>
2026-01-20 10:09:20 -0700

    Fixed age_guess.py with while loop functionality

--- age_guess.py
+++ age_guess.py
@@ -3,10 +3,15 @@ import random
 def guess_age():
     print("Hey!  I'm going to try and guess your age.")
     name = input("What's your name?")
-    correct_or_not = input(f"I'm going to guess your age is {random.randrange(15, 31)}!  Is that right? (y/n)")
-    if correct_or_not == 'y':
-        print(f"Yay! ! got it right!"
-        return
-    else:
-        print("rats!")
     
+    while True:
+        age = random.randint(15, 30)
+        answer = input(f"Is your age {age}? (y/n): :")
+
+        if answer == 'y':
+            print(f"Yes! {name} is {age} years old.")
+            break
+        else:
+            print("Rats...")
+   
+guess_age()
